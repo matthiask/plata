602775cae30ac1c0aedb414238ae49e96812360b
Matthias Kestenholz <mk@spinlock.ch>
2010-04-20 10:01:24 +0200

    Move contact model to correct module

--- pasta/contact/models.py
+++ pasta/contact/models.py
@@ -23,64 +23,36 @@ class ContactManager(models.Manager):
 
 
 class Contact(models.Model):
+    user = models.ForeignKey(User, verbose_name=_('user'), blank=True, null=True)
+    email = models.EmailField(_('e-mail address'), unique=True)
     created = models.DateTimeField(_('created'), default=datetime.now)
 
-    user = models.ForeignKey(User, verbose_name=_('user'), related_name='contacts',
-        blank=True, null=True)
+    billing_company = models.CharField(_('company'), max_length=100, blank=True)
+    billing_first_name = models.CharField(_('first name'), max_length=100, blank=True)
+    billing_last_name = models.CharField(_('last name'), max_length=100, blank=True)
+    billing_address = models.TextField(_('address'), blank=True)
+    billing_zip_code = models.CharField(_('ZIP code'), max_length=50, blank=True)
+    billing_city = models.CharField(_('city'), max_length=100, blank=True)
+    billing_country = models.CharField(_('country'), max_length=2, blank=True,
+        help_text=_('ISO2 code'))
 
-    first_name = models.CharField(_('first name'), max_length=100)
-    last_name = models.CharField(_('last name'), max_length=100)
+    shipping_same_as_billing = models.BooleanField(_('shipping address equals billing address'),
+        default=True)
 
-    manner_of_address = models.CharField(_('Manner of address'), max_length=30,
-        blank=True, default='', help_text=_('e.g. Mr., Ms., Dr.'))
-    title = models.CharField(_('Title/auxiliary'), max_length=100, blank=True,
-        default='', help_text=_('e.g. MSc ETH'))
+    shipping_company = models.CharField(_('company'), max_length=100, blank=True)
+    shipping_first_name = models.CharField(_('first name'), max_length=100, blank=True)
+    shipping_last_name = models.CharField(_('last name'), max_length=100, blank=True)
+    shipping_address = models.TextField(_('address'), blank=True)
+    shipping_zip_code = models.CharField(_('ZIP code'), max_length=50, blank=True)
+    shipping_city = models.CharField(_('city'), max_length=100, blank=True)
+    shipping_country = models.CharField(_('country'), max_length=2, blank=True,
+        help_text=_('ISO2 code'))
+
+    currency = models.CharField(_('currency'), max_length=10)
 
     class Meta:
-        ordering = ['last_name', 'first_name']
         verbose_name = _('contact')
         verbose_name_plural = _('contacts')
 
-    objects = ContactManager()
-
     def __unicode__(self):
-        return u'%s %s' % (self.first_name, self.last_name)
-
-
-class Address(models.Model):
-    contact = models.ForeignKey(Contact, related_name='addresses',
-        verbose_name=_('Person'))
-
-    is_billing = models.BooleanField(_('billing'))
-    is_shipping = models.BooleanField(_('shipping'))
-
-    email = models.EmailField(_('e-mail address'), blank=True)
-    website = models.URLField(verify_exists=False, blank=True)
-    function = models.TextField(_('function'), blank=True)
-    phone = models.CharField(_('phone'), max_length=30, blank=True,
-        help_text=_('Please include the country prefix, but no spaces, e.g. +41555111141'))
-    fax = models.CharField(_('fax'), max_length=30, blank=True,
-        help_text=_('Please include the country prefix, but no spaces, e.g. +41555111141'))
-    mobile = models.CharField(_('mobile'), max_length=30, blank=True,
-        help_text=_('Please include the country prefix, but no spaces, e.g. +41555111141'))
-    address = models.TextField(_('address'), blank=True)
-
-    zip_code = models.CharField(_('ZIP Code'), max_length=30, blank=True)
-    city = models.CharField(_('city'), max_length=30, blank=True)
-
-    region = models.CharField(_('region'), max_length=30, blank=True)
-    country = models.CharField(_('country'), max_length=30, blank=True,
-        default='CH')
-
-    class Meta:
-        verbose_name = _('address')
-        verbose_name_plural = _('addresses')
-
-    def __unicode__(self):
-        types = []
-        if self.is_billing:
-            types.append(ugettext('billing'))
-        if self.is_shipping:
-            types.append(ugettext('shipping'))
-
-        return (types and ugettext(' and ').join(types) + u' ' or u'') + (_('address of %s') % self.contact)
+        return u'%s %s' % (self.billing_first_name, self.billing_last_name)
--- pasta/shop/models.py
+++ pasta/shop/models.py
@@ -13,43 +13,8 @@ from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 
 from pasta import pasta_settings
+from pasta.contact.models import Contact
 from pasta.product.models import Product
-
-
-class Contact(models.Model):
-    user = models.ForeignKey(User, verbose_name=_('user'), blank=True, null=True)
-    email = models.EmailField(_('e-mail address'), unique=True)
-    created = models.DateTimeField(_('created'), default=datetime.now)
-
-    billing_company = models.CharField(_('company'), max_length=100, blank=True)
-    billing_first_name = models.CharField(_('first name'), max_length=100, blank=True)
-    billing_last_name = models.CharField(_('last name'), max_length=100, blank=True)
-    billing_address = models.TextField(_('address'), blank=True)
-    billing_zip_code = models.CharField(_('ZIP code'), max_length=50, blank=True)
-    billing_city = models.CharField(_('city'), max_length=100, blank=True)
-    billing_country = models.CharField(_('country'), max_length=2, blank=True,
-        help_text=_('ISO2 code'))
-
-    shipping_same_as_billing = models.BooleanField(_('shipping address equals billing address'),
-        default=True)
-
-    shipping_company = models.CharField(_('company'), max_length=100, blank=True)
-    shipping_first_name = models.CharField(_('first name'), max_length=100, blank=True)
-    shipping_last_name = models.CharField(_('last name'), max_length=100, blank=True)
-    shipping_address = models.TextField(_('address'), blank=True)
-    shipping_zip_code = models.CharField(_('ZIP code'), max_length=50, blank=True)
-    shipping_city = models.CharField(_('city'), max_length=100, blank=True)
-    shipping_country = models.CharField(_('country'), max_length=2, blank=True,
-        help_text=_('ISO2 code'))
-
-    currency = models.CharField(_('currency'), max_length=10)
-
-    class Meta:
-        verbose_name = _('contact')
-        verbose_name_plural = _('contacts')
-
-    def __unicode__(self):
-        return u'%s %s' % (self.billing_first_name, self.billing_last_name)
 
 
 class Order(models.Model):
