88969c121a3771c9e4f842493a2ee8861dc0fc08
Matthias Kestenholz <mk@spinlock.ch>
2010-05-19 13:15:58 +0200

    FeinCMS products: Use regions as translations

--- plata/product/feincms/models.py
+++ plata/product/feincms/models.py
@@ -2,20 +2,30 @@ from datetime import date
 
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language, ugettext_lazy as _
 
-from feincms.models import Base
+from feincms.models import Base, ContentProxy
 
 from plata.product.models import Product
 
 
+class RegionTranslationContentProxy(ContentProxy):
+    def get_content(self, item, attr):
+        try:
+            region = item.template.regions_dict['%s_%s' % (attr, get_language()[:2])]
+        except KeyError:
+            return []
+
+        return sorted(item._content_for_region(region), key=lambda c: c.ordering)
+
+
 class CMSProduct(Product, Base):
-    pass
+    content_proxy_class = RegionTranslationContentProxy
 
 CMSProduct.register_regions(
-    ('german', _('German')),
-    ('english', _('English')),
-    ('french', _('French')),
+    ('main_de', _('German')),
+    ('main_en', _('English')),
+    ('main_fr', _('French')),
     )
 
 from feincms.content.raw.models import RawContent
