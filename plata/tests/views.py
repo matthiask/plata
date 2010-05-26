d758a11e25a81b0798d355644cb2e86203dd819b
Matthias Kestenholz <mk@spinlock.ch>
2010-05-26 08:41:17 +0200

    Tests: Create stock transaction period a bit earlier

--- plata/tests/views.py
+++ plata/tests/views.py
@@ -157,11 +157,12 @@ class ViewTest(PlataTest):
         self.assertEqual(self.client.post('/confirmation/', {}).status_code, 200)
         self.assertEqual(Order.objects.get(pk=order.id).status, Order.CHECKOUT)
 
+        Period.objects.create(name='Test period')
+
         self.assertContains(self.client.post('/confirmation/', {
             'payment_method': 'plata.payment.modules.postfinance',
             }), 'SHASign')
 
-        Period.objects.create(name='Test period')
         self.assertRedirects(self.client.post('/confirmation/', {
             'payment_method': 'plata.payment.modules.cod',
             }), '/order/success/')
