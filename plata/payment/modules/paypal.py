d5bc7fe7fcbd9ee902d9f438bee1d7084ce8f8b9
Matthias Kestenholz <mk@spinlock.ch>
2010-05-27 20:36:47 +0200

    First successful payment through paypal sandbox
    Amounts are wrong though.

--- plata/payment/modules/paypal.py
+++ plata/payment/modules/paypal.py
@@ -1,8 +1,14 @@
+from datetime import datetime
+from decimal import Decimal
 import urllib
 import urllib2
+import sys
 
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseServerError
+from django.http import HttpResponse, HttpResponseForbidden,\
+    HttpResponseServerError
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import csrf_exempt
@@ -29,6 +35,7 @@ class PaymentProcessor(ProcessorBase):
         PAYPAL = settings.PAYPAL
 
         if order.is_paid():
+            # TODO maybe create stock transactions?
             return redirect('plata_order_already_paid')
 
         payment = order.payments.create(
@@ -41,14 +48,17 @@ class PaymentProcessor(ProcessorBase):
             type=StockTransaction.PAYMENT_PROCESS_RESERVATION,
             negative=True, payment=payment)
 
-        raise NotImplementedError, 'This is not implemented yet.'
+        if PAYPAL['LIVE']:
+            PP_URL = "https://www.paypal.com/cgi-bin/webscr"
+        else:
+            PP_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr"
 
         return render_to_response('payment/paypal_form.html', {
             'order': order,
+            'payment': payment,
             'HTTP_HOST': request.META.get('HTTP_HOST'),
-            'form_params': form_params,
+            'post_url': PP_URL,
             }, context_instance=RequestContext(request))
-
 
     @method_decorator(csrf_exempt)
     @method_decorator(require_POST)
@@ -62,6 +72,8 @@ class PaymentProcessor(ProcessorBase):
 
         parameters = None
 
+        sys.stderr.write('stage 1');sys.stderr.flush()
+
         try:
             if request.POST.get('payment_status') == 'Completed':
                 if request.POST:
@@ -72,17 +84,28 @@ class PaymentProcessor(ProcessorBase):
                 pass
                 #log_error("IPN", "The parameter payment_status was not Completed.")
 
+            sys.stderr.write('stage 2');sys.stderr.flush()
+
             if parameters:
+                sys.stderr.write(repr(parameters).encode('utf-8'))
+                sys.stderr.flush()
+
                 parameters['cmd']='_notify-validate'
 
-                params = urllib.urlencode(parameters)
+                #parameters = dict([(k, v.encode(parameters['charset'])) for k, v in parameters.items()])
+
+                params = urllib.urlencode(parameters) #.decode(parameters['charset'])
                 req = urllib2.Request(PP_URL, params)
                 req.add_header("Content-type", "application/x-www-form-urlencoded")
+                sys.stderr.write('stage 3');sys.stderr.flush()
                 response = urllib2.urlopen(req)
                 status = response.read()
+                sys.stderr.write('STATUS %r' % status)
                 if not status == "VERIFIED":
                     #print "The request could not be verified, check for fraud." + str(status)
                     parameters = None
+
+            sys.stderr.write('stage 4');sys.stderr.flush()
 
             if parameters:
                 reference = parameters['txn_id']
@@ -93,13 +116,48 @@ class PaymentProcessor(ProcessorBase):
                 email = parameters['payer_email']
                 identifier = parameters['payer_id']
 
-                # DO SOMETHING WITH THE PARAMETERS HERE, STORE THEM, ETC...
+                sys.stderr.write('stage 5');sys.stderr.flush()
+
+                try:
+                    order, order_id, payment_id = invoice_id.split('-')
+                except ValueError:
+                    return HttpResponseForbidden('Malformed order ID')
+
+                sys.stderr.write('stage 6');sys.stderr.flush()
+
+                order = get_object_or_404(self.shop.order_model, pk=order_id)
+                try:
+                    payment = order.payments.get(pk=payment_id)
+                except order.payments.model.DoesNotExist:
+                    payment = order.payments.model(
+                        order=order,
+                        payment_module=u'%s' % self.name,
+                        )
+
+                sys.stderr.write('stage 7');sys.stderr.flush()
+                payment.currency = currency
+                payment.amount = Decimal(amount)
+                payment.data_json = request.POST.copy()
+                payment.transaction_id = reference
+                #payment.payment_method = BRAND
+                #payment.notes = STATUS_DICT.get(STATUS)
+                payment.authorized = datetime.now()
+
+                sys.stderr.write('stage 8');sys.stderr.flush()
+                payment.save()
+                sys.stderr.write('stage 9');sys.stderr.flush()
+
 
                 return HttpResponse("Ok")
 
         except Exception, e:
-            import sys
+            import traceback
             sys.stderr.write(unicode(e))
             sys.stderr.flush()
+            traceback.print_exc(100, sys.stderr)
+            sys.stderr.flush()
+
+        sys.stderr.write('aaaraaaaaaaaaaaaaaaaaa')
+        sys.stderr.flush()
 
         return HttpResponseServerError()
