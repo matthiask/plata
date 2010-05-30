09c225866538a053cdb0a3ecb07ecf3726aa88b2
Matthias Kestenholz <mk@spinlock.ch>
2010-05-30 10:55:23 +0200

    JSON support: Use decimal.Decimal to parse floats

--- plata/utils.py
+++ plata/utils.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.core.serializers.json import DjangoJSONEncoder
 from django.utils import simplejson
 
@@ -10,7 +12,8 @@ class JSONFieldDescriptor(object):
         cache_field = '_cached_jsonfield_%s' % self.field
         if not hasattr(obj, cache_field):
             try:
-                setattr(obj, cache_field, simplejson.loads(getattr(obj, self.field)))
+                setattr(obj, cache_field, simplejson.loads(getattr(obj, self.field),
+                    parse_float=Decimal))
             except (TypeError, ValueError):
                 setattr(obj, cache_field, {})
         return getattr(obj, cache_field)
