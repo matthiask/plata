e039f9d7200fe1c78fd6e7ba63cc49e83b130f55
Matthias Kestenholz <mk@spinlock.ch>
2010-05-30 13:57:23 +0200

    Make sure product prices and variations cannot be deleted through standard means

--- plata/shop/admin.py
+++ plata/shop/admin.py
@@ -6,7 +6,7 @@ from . import models
 
 class OrderItemInline(admin.TabularInline):
     model = models.OrderItem
-    raw_id_fields = ('variation',)
+    raw_id_fields = ('variation', 'product_price')
 
 class AppliedDiscountInline(admin.TabularInline):
     model = models.AppliedDiscount
