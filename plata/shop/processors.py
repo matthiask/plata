aee644b9b7f8de57590327d086fbc218b1f3f4be
Matthias Kestenholz <mk@spinlock.ch>
2010-05-31 13:25:04 +0200

    Order processors: Rename instance to order, because that's what it is

--- plata/shop/processors.py
+++ plata/shop/processors.py
@@ -36,13 +36,13 @@ class ProcessorBase(object):
             return dic.get(key)
         return dic
 
-    def process(self, instance, items):
+    def process(self, order, items):
         raise NotImplementedError
 
 
 class InitializeOrderProcessor(ProcessorBase):
-    def process(self, instance, items):
-        instance.items_subtotal = instance.items_tax = instance.items_discount = 0
+    def process(self, order, items):
+        order.items_subtotal = order.items_tax = order.items_discount = 0
 
         for item in items:
             # Recalculate item stuff
@@ -51,13 +51,13 @@ class InitializeOrderProcessor(ProcessorBase):
 
 
 class DiscountProcessor(ProcessorBase):
-    def process(self, instance, items):
-        for applied in instance.applied_discounts.all():
-            applied.apply(instance, items)
+    def process(self, order, items):
+        for applied in order.applied_discounts.all():
+            applied.apply(order, items)
 
 
 class TaxProcessor(ProcessorBase):
-    def process(self, instance, items):
+    def process(self, order, items):
         for item in items:
             taxable = item._line_item_price - (item._line_item_discount or 0)
             item._line_item_tax = taxable * item.tax_class.rate/100
@@ -65,45 +65,45 @@ class TaxProcessor(ProcessorBase):
 
 
 class ItemSummationProcessor(ProcessorBase):
-    def process(self, instance, items):
+    def process(self, order, items):
         for item in items:
-            instance.items_subtotal += item._line_item_price
-            instance.items_discount += item._line_item_discount or 0
-            instance.items_tax += item._line_item_tax
+            order.items_subtotal += item._line_item_price
+            order.items_discount += item._line_item_discount or 0
+            order.items_tax += item._line_item_tax
 
         self.set_processor_value('total', 'items',
-            instance.items_subtotal - instance.items_discount + instance.items_tax)
+            order.items_subtotal - order.items_discount + order.items_tax)
 
 
 class ShippingProcessor(ProcessorBase):
-    def process(self, instance, items):
-        instance.shipping_tax = 0
+    def process(self, order, items):
+        order.shipping_tax = 0
 
         subtotal = 0
 
-        if instance.shipping_cost:
-            subtotal += instance.shipping_cost
-        if instance.shipping_discount:
-            subtotal -= instance.shipping_discount
+        if order.shipping_cost:
+            subtotal += order.shipping_cost
+        if order.shipping_discount:
+            subtotal -= order.shipping_discount
 
         subtotal = max(subtotal, 0)
 
         # TODO move this into shipping processor
-        instance.shipping_tax = subtotal * Decimal('0.076')
+        order.shipping_tax = subtotal * Decimal('0.076')
 
         self.set_processor_value('total', 'shipping',
-            subtotal + instance.shipping_tax)
+            subtotal + order.shipping_tax)
 
 
 class OrderSummationProcessor(ProcessorBase):
-    def process(self, instance, items):
+    def process(self, order, items):
         """
         The value must be quantized here, because otherwise f.e. the payment
         modules will be susceptible to rounding errors giving f.e. missing
         payments of 0.01 units.
         """
 
-        instance.total = sum(
+        order.total = sum(
             self.get_processor_value('total').values(),
             Decimal('0.00'),
             ).quantize(Decimal('0.00'))
