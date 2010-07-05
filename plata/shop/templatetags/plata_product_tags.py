8bc6fddeb1232e7973d76bef05252f8228070ab7
Matthias Kestenholz <mk@spinlock.ch>
2010-07-05 11:22:26 +0200

    Add template tag to get featured products for a list of categories

---
+++ plata/shop/templatetags/plata_product_tags.py
@@ -0,0 +1,23 @@
+from django import template
+
+import plata
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def featured_products_for_categories(category_list, variable_name='featured_product'):
+    """
+    {% featured_products_for_categories category_list "variable_name" %}
+    """
+
+    category_list = list(category_list)
+
+    for category in category_list:
+        try:
+            setattr(category, variable_name, category.products.featured()[0])
+        except IndexError:
+            pass
+
+    return u''
