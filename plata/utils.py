3f2b4236bdb5199d4830a893c7b511f7875dc501
Matthias Kestenholz <mk@spinlock.ch>
2011-01-03 14:40:44 +0100

    Make working with JSONDataDescriptor easier

--- plata/utils.py
+++ plata/utils.py
@@ -10,6 +10,24 @@ except TypeError:
     raise Exception('simplejson>=2.1 with support for use_decimal required.')
 
 
+class CallbackOnUpdateDict(dict):
+    """Dict which executes a callback on every update"""
+
+    def __init__(self, *args, **kwargs):
+        self.callback = kwargs.pop('callback')
+        super(CallbackOnUpdateDict, self).__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        ret = super(CallbackOnUpdateDict, self).__setitem__(key, value)
+        self.callback(self)
+        return ret
+
+    def update(self, d):
+        ret = super(CallbackOnUpdateDict, self).update(d)
+        self.callback(self)
+        return ret
+
+
 class JSONFieldDescriptor(object):
     def __init__(self, field):
         self.field = field
@@ -18,13 +36,20 @@ class JSONFieldDescriptor(object):
         cache_field = '_cached_jsonfield_%s' % self.field
         if not hasattr(obj, cache_field):
             try:
-                setattr(obj, cache_field, simplejson.loads(getattr(obj, self.field),
-                    use_decimal=True))
+                value = simplejson.loads(getattr(obj, self.field), use_decimal=True)
             except (TypeError, ValueError):
-                setattr(obj, cache_field, {})
+                value = {}
+
+            self.__set__(obj, value)
+
         return getattr(obj, cache_field)
 
     def __set__(self, obj, value):
+        if not isinstance(value, CallbackOnUpdateDict):
+            value = CallbackOnUpdateDict(value,
+                 # Update cached and serialized value on every write to the data dict
+                callback=lambda d: self.__set__(obj, d))
+
         setattr(obj, '_cached_jsonfield_%s' % self.field, value)
         setattr(obj, self.field, simplejson.dumps(value, use_decimal=True,
             cls=DjangoJSONEncoder))
