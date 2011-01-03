1a01ae60b38f3646b4ff5580b455d7e7e45ed888
Matthias Kestenholz <mk@spinlock.ch>
2011-01-03 14:43:10 +0100

    Move jsonize into plata.utils

--- plata/discount/admin.py
+++ plata/discount/admin.py
@@ -2,33 +2,14 @@ from threading import local
 
 from django import forms
 from django.contrib import admin
-from django.db.models import Model
 from django.utils.translation import ugettext_lazy as _
 
-from . import models
+from plata.discount import models
+from plata.utils import jsonize
 
 
 # Internal state tracking helper for config fields
 _discount_admin_state = local()
-
-
-def jsonize(v):
-    """
-    Convert the discount configuration into a state in which it can be
-    stored inside the JSON field.
-
-    Some information is lost here; f.e. we only store the primary key
-    of model objects, so you have to remember yourself which objects
-    are meant by the primary key values.
-    """
-
-    if isinstance(v, dict):
-        return dict((i1, jsonize(i2)) for i1, i2 in v.items())
-    if hasattr(v, '__iter__'):
-        return [jsonize(i) for i in v]
-    if isinstance(v, Model):
-        return v.pk
-    return v
 
 
 class DiscountAdminForm(forms.ModelForm):
--- plata/utils.py
+++ plata/utils.py
@@ -2,12 +2,32 @@ from decimal import Decimal
 import simplejson
 
 from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Model
 
 
 try:
     simplejson.dumps([42], use_decimal=True)
 except TypeError:
     raise Exception('simplejson>=2.1 with support for use_decimal required.')
+
+
+def jsonize(v):
+    """
+    Convert the discount configuration into a state in which it can be
+    stored inside the JSON field.
+
+    Some information is lost here; f.e. we only store the primary key
+    of model objects, so you have to remember yourself which objects
+    are meant by the primary key values.
+    """
+
+    if isinstance(v, dict):
+        return dict((i1, jsonize(i2)) for i1, i2 in v.items())
+    if hasattr(v, '__iter__'):
+        return [jsonize(i) for i in v]
+    if isinstance(v, Model):
+        return v.pk
+    return v
 
 
 class CallbackOnUpdateDict(dict):
