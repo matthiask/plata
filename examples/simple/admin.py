ef8e323697299fcc494c1e0819a830ddf5313f8e
Matthias Kestenholz <mk@spinlock.ch>
2011-03-24 17:53:35 +0100

    Simple example: Product price admin is not strictly necessary, inlines are enough

--- examples/simple/admin.py
+++ examples/simple/admin.py
@@ -18,21 +18,3 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
 
 admin.site.register(models.Product, ProductAdmin)
-
-
-class ReadonlyModelAdmin(admin.ModelAdmin):
-    actions = None # no "delete selected objects" action
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-# All fields are read only; these models are only used for raw_id_fields support
-admin.site.register(models.ProductPrice,
-    admin_class=ReadonlyModelAdmin,
-    list_display=('__unicode__', 'product', 'currency', '_unit_price', 'tax_included',
-        'tax_class', 'is_active', 'valid_from', 'valid_until', 'is_sale'),
-    list_filter=('is_active', 'is_sale', 'tax_included', 'tax_class', 'currency'),
-    readonly_fields=('product', 'currency', '_unit_price', 'tax_included', 'tax_class',
-        'is_active', 'valid_from', 'valid_until', 'is_sale'),
-    search_fields=('product__name', 'product__description', '_unit_price'),
-    can_delete=False,
-    )
