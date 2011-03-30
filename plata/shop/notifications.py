62a80896a635ac5ae9fdbfa9deb57132b44067a4
Matthias Kestenholz <mk@spinlock.ch>
2011-03-30 13:47:50 +0200

    Add API docs for the notifications module

--- plata/shop/notifications.py
+++ plata/shop/notifications.py
@@ -35,6 +35,23 @@ class EmailHandler(object):
 
 
 class ContactCreatedHandler(EmailHandler):
+    """
+    Send an e-mail message to a newly created contact, optionally BCC'ing
+    the addresses passed as ``always_bcc`` upon handler initialization.
+
+    Usage::
+
+        signals.contact_created.connect(
+            ContactCreatedHandler(),
+            weak=False)
+
+    or::
+
+        signals.contact_created.connect(
+            ContactCreatedHandler(always_bcc=['owner@example.com']),
+            weak=False)
+    """
+
     def message(self, sender, contact, **kwargs):
         email = render_to_string('plata/notifications/contact_created.txt',
             self.context(kwargs)).splitlines()
@@ -47,6 +64,18 @@ class ContactCreatedHandler(EmailHandler):
 
 
 class SendInvoiceHandler(EmailHandler):
+    """
+    Send an e-mail with attached invoice to the customer after successful
+    order completion, optionally BCC'ing the addresses passed as ``always_bcc``
+    to the handler upon initialization.
+
+    Usage::
+
+        signals.order_completed.connect(
+            SendInvoiceHandler(always_bcc=['owner@example.com']),
+            weak=False)
+    """
+
     def message(self, sender, order, **kwargs):
         email = render_to_string('plata/notifications/order_completed.txt',
             self.context(kwargs)).splitlines()
@@ -66,6 +95,19 @@ class SendInvoiceHandler(EmailHandler):
 
 
 class SendPackingSlipHandler(EmailHandler):
+    """
+    Send an e-mail with attached packing slip to the addresses specified upon
+    handler initialization. You should pass at least one address in either
+    the ``always_to`` or the ``always_bcc`` argument, or else the e-mail
+    will go nowhere.
+
+    Usage::
+
+        signals.order_completed.connect(
+            SendPackingSlipHandler(always_to=['warehouse@example.com']),
+            weak=False)
+    """
+
     def message(self, sender, order, **kwargs):
         email = render_to_string('plata/notifications/packing_slip.txt',
             self.context(kwargs)).splitlines()
