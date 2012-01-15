e1677f3387c7a8f91a97c704548f842e16e9e161
Matthias Kestenholz <mk@spinlock.ch>
2012-01-15 15:32:24 +0100

    Deprecate the product modules
    Development of product modules should continue outside of the core.

---
+++ plata/product/modules/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+
+warnings.warn("Plata's product modules have been deprecated. Write your"
+    " own product module instead using the built-in tools or use one of the"
+    " upcoming external product modules.",
+    DeprecationWarning, stacklevel=2)
