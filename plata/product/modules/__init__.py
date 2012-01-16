32f3b25793004aa88386706a87b62b826071e7dc
Matthias Kestenholz <mk@spinlock.ch>
2012-01-16 14:32:53 +0100

    Remove remains of modules

--- plata/product/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-warnings.warn("Plata's product modules have been deprecated. Write your"
-    " own product module instead using the built-in tools or use one of the"
-    " upcoming external product modules.",
-    DeprecationWarning, stacklevel=2)
