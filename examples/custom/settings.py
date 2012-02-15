445d69dc7fb5b1346c3052e88404c000676b1c55
Matthias Kestenholz <mk@spinlock.ch>
2012-02-15 12:30:17 +0100

    Custom example: Provide a custom checkout template

--- examples/custom/settings.py
+++ examples/custom/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'custom.urls'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
