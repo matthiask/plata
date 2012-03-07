e9cabc32f991cb9f6a8ed89c8a00b066af2710c8
Matthias Kestenholz <mk@spinlock.ch>
2012-03-07 11:52:43 +0100

    Initialize checkout form if no contact exists but user is already authenticated

--- plata/contact/forms.py
+++ plata/contact/forms.py
@@ -35,10 +35,16 @@ class CheckoutForm(shop_forms.BaseCheckoutForm):
                 initial['shipping_%s' % f] = getattr(contact, 'shipping_%s' % f)
 
             kwargs['initial'] = initial
+        elif request.user.is_authenticated():
+            kwargs['initial'] = {
+                'email': request.user.email,
+                'billing_first_name': request.user.first_name,
+                'billing_last_name': request.user.last_name,
+                }
 
         super(CheckoutForm, self).__init__(*args, **kwargs)
 
-        if not contact:
+        if not (contact or request.user.is_authenticated()):
             self.fields['create_account'] = forms.BooleanField(
                 label=_('create account'),
                 required=False, initial=True)
