987c54559cb52370fc459a30cdbdfd0e38c5ef62
Matthias Kestenholz <mk@spinlock.ch>
2012-09-28 15:07:23 +0200

    Add the variable `plata.price_includes_tax` to the template context

--- plata/context_processors.py
+++ plata/context_processors.py
@@ -7,6 +7,7 @@ def plata_context(request):
     * ``plata.shop``: The current :class:`plata.shop.views.Shop` instance
     * ``plata.order``: The current order
     * ``plata.contact``: The current contact instance
+    * ``plata.price_includes_tax``: Whether prices include tax or not
     """
 
     shop = plata.shop_instance()
@@ -17,4 +18,5 @@ def plata_context(request):
         'shop': shop,
         'order': shop.order_from_request(request),
         'contact': shop.contact_from_user(request.user),
+        'price_includes_tax': plata.settings.PLATA_PRICE_INCLUDES_TAX,
         }}
