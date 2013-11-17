8a3ac13f1fc350b4699dc552d79abe4159aee3ac
Matthias Kestenholz <mk@spinlock.ch>
2013-11-17 17:04:21 +0100

    Compatibility with discovery test runner

--- tests/testapp/tests/__init__.py
+++ tests/testapp/tests/__init__.py
@@ -1,5 +1,7 @@
+# flake8: noqa
+
 from __future__ import absolute_import
 
-from .admin import AdminTest
-from .models import ModelTest
-from .views import ViewTest
+from .test_admin import AdminTest
+from .test_models import ModelTest
+from .test_views import ViewTest
