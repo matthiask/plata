f9c12e0fc3b93788eaf8d4e016a62b51a1022f79
Matthias Kestenholz <mk@spinlock.ch>
2013-11-18 10:36:27 +0100

    Switch to setuptools and proper requirements handling
    Fixes #64.

--- setup.py
+++ setup.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 import os
-import setuplib
+from setuptools import setup, find_packages
 
-packages, package_data = setuplib.find_packages('plata')
 
-setup(name='Plata',
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='Plata',
     version=__import__('plata').__version__,
     description='Plata - the lean and mean Django-based Shop',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    long_description=read('README.rst'),
     author='Matthias Kestenholz',
     author_email='mk@feinheit.ch',
     url='https://github.com/matthiask/plata/',
     license='BSD License',
     platforms=['OS Independent'],
-    packages=packages,
-    package_data=package_data,
+    packages=find_packages(
+        exclude=[],
+    ),
+    package_data={
+        '': ['*.html', '*.txt'],
+        'plata': [
+            'locale/*/*/*.*',
+        ],
+        'plata.shop': [
+            'templates/*.*',
+            'templates/*/*.*',
+            'templates/*/*/*.*',
+            'templates/*/*/*/*.*',
+        ],
+    },
+    install_requires=[
+        'Django>=1.4.2',
+        'simplejson>=3.0',
+        'openpyxl>=1.6',
+        'reportlab>=2.6',
+        'pdfdocument>=1.6',
+        'django-countries>=1.5',
+        'pytz',
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -25,8 +50,15 @@ setup(name='Plata',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # Our dependencies do not support Python 3 either... :-(
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
     ],
+    zip_safe=False,
 )
