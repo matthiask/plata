7500c45838d220cc37bfe359771e15cec216868c
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-04-30 12:04:14 +0200

    add downloadable pdf invoice for users

--- plata/reporting/views.py
+++ plata/reporting/views.py
@@ -1,4 +1,6 @@
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 
 from pdfdocument.utils import pdf_response
@@ -27,6 +29,19 @@ def invoice_pdf(request, order_id):
     plata.reporting.order.invoice_pdf(pdf, order)
     return response
 
+@login_required
+def invoice(request, order_id):
+    """
+    Returns the invoice PDF
+    """
+    order = get_object_or_404(plata.shop_instance().order_model, pk=order_id)
+
+    if order in request.user.orders.all():
+        pdf, response = pdf_response('invoice-%09d' % order.id)
+        plata.reporting.order.invoice_pdf(pdf, order)
+        return response
+    else:
+        raise Http404
 
 @staff_member_required
 def packing_slip_pdf(request, order_id):
