a958afc57069a7f612cf728502dc7ab312a73db1
leandrafinger <leandra.finger@gmail.com>
2016-03-04 08:16:18 +0100

    fix

--- plata/payment/modules/base.py
+++ plata/payment/modules/base.py
@@ -167,7 +167,7 @@ class ProcessorBase(object):
                 except Discount.DoesNotExist:
                     return
 
-                if discount.allowed_uses and discount.used < discount.allowed_uses:
+                if discount.used < discount.allowed_uses or not discount.allowed_uses:
                     discount.used += 1
                     discount.save()
                 else:
