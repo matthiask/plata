fed0ba2b836edbfc61b813c33164e62baa936a9a
Henning Hraban Ramm <hraban@fiee.net>
2017-04-10 14:12:20 +0200

    fix/modernize url setup

--- examples/custom/urls.py
+++ examples/custom/urls.py
@@ -1,30 +1,29 @@
 import os
-
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.shortcuts import redirect
-
-from custom.views import shop
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from custom.views import shop, product_list, product_detail
 
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'', include(shop.urls)),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', lambda request: redirect('plata_product_list')),
-    url(r'^products/$', 'custom.views.product_list',
+    url(r'^products/$', product_list,
         name='plata_product_list'),
-    url(r'^products/(?P<object_id>\d+)/$', 'custom.views.product_detail',
+    url(r'^products/(?P<object_id>\d+)/$', product_detail,
         name='plata_product_detail'),
 
     url(r'^reporting/', include('plata.reporting.urls')),
 
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media/(?P<path>.*)$', serve,
         {'document_root': os.path.join(os.path.dirname(__file__), 'media/')}),
-)
+]
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns += staticfiles_urlpatterns()
--- examples/simple/urls.py
+++ examples/simple/urls.py
@@ -1,30 +1,31 @@
 import os
-
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.shortcuts import redirect
-
-from simple.views import shop
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from simple.views import shop, product_list, product_detail
 
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'', include(shop.urls)),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', lambda request: redirect('plata_product_list')),
-    url(r'^products/$', 'simple.views.product_list',
+    url(r'^$', lambda request: redirect('plata_product_list'), name='plata_home'),
+    url(r'^dashboard/$', lambda request: redirect('plata_product_list'), name='dashboard'),
+
+    url(r'^products/$', product_list,
         name='plata_product_list'),
-    url(r'^products/(?P<object_id>\d+)/$', 'simple.views.product_detail',
+    url(r'^products/(?P<object_id>\d+)/$', product_detail,
         name='plata_product_detail'),
 
-    url(r'^reporting/', include('plata.reporting.urls')),
+    url(r'^reporting/', include('plata.reporting.urls',)),
 
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media/(?P<path>.*)$', serve,
         {'document_root': os.path.join(os.path.dirname(__file__), 'media/')}),
-)
+]
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns += staticfiles_urlpatterns()
